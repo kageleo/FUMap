bfc77dd8f0255df668673914da98d74c3cb1d342
kagereo <60954189+kagereo@users.noreply.github.com>
2021-12-16 11:58:21 +0900

    Update FUMap.js

--- FUMap.js
+++ FUMap.js
@@ -1,31 +1,34 @@
 function pin(X, Y){
-    // var textboxX = document.getElementById("textBoxX");
-    // var textboxY = document.getElementById("textBoxY");
-    
-    // var X = textboxX.value;
-    // var Y = textboxY.value;
 
     document.getElementById( 'pin-point' ).style.left = X + "%";
     document.getElementById( 'pin-point' ).style.top = Y + "%";
     document.getElementById( 'pin-point' ).style.opacity = 1;
 }
 
+function str_sch(str, sch){
+    if(!str.indexOf(sch)){
+        return 
+    }
+}
+
 function search(){
     var search_text = document.getElementById("search-text");
-    var search_word = search_text.value
+    var search_word = search_text.value;
     switch (search_word){
         case "A棟":
-            pin(68, 50)
+        case "a棟":
+        case "A":
+        case "a":
+            pin(68, 49);
             break;
         case "８号館":
         case "8号館":
-            pin(55, 65)
+        case "8":
+        case "８":
+            pin(56, 64);
             break;
         default:
-            console.log("該当なし");
+            document.getElementById('pin-point').style.opacity = 0;
+            document.getElementById('search-text').value = "該当なし ";
     }
-    // if (search_word == "A棟")
-    //     pin(470, 180)
-    // else if (search_word == "８号館")
-    //     pin(390, 240)
 }
